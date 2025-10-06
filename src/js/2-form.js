aa77201bf5f92e1aeae1e4d47f6f4609b940a78c
Elizabeth Shkinder <elizabethshkinder@elizabeths-air.home>
2025-10-06 11:20:04 +0100

    implement feedback form state from task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -9,18 +9,55 @@ const form = document.querySelector (".feedback-form");
 const emailEL = form.elements.email;
 const messageEl = form.elements.message;
 
+form.addEventListener("input", inputAction);
+form.addEventListener("submit", submitAction);
 
-emailEL.addEventListener("focus", () => {
-  emailEL.setAttribute("placeholder", "Type area");
-});
-emailEL.addEventListener("blur", () => {
-  emailEL.removeAttribute("placeholder");
-});
+formRestore();
 
-messageEl.addEventListener("focus", () => {
-  messageEl.setAttribute("placeholder", "Type area");
-});
-messageEl.addEventListener("blur", () => {
-  messageEl.removeAttribute("placeholder");
-});
+function inputAction(event){
+    const {name, value} = event.target;
+
+    if (!(name in formData)) return;
+
+    formData[name] = value.trim();
+
+    localStorage.setItem(key, JSON.stringify(formData));
+}
+
+function submitAction(event) {
+    event.preventDefault();
+
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+
+    if(!email || !message){
+        alert("Fill please all fields")
+        return;
+    }
+
+    console.log({email, message});
+
+    localStorage.removeItem(key);
+    formData.email = "";
+    formData.message = "";
+    form.reset();
+}
+
+function formRestore(){
+    try {
+        const data = localStorage.getItem(key);
+        if(!data) return;
+
+        const saved = JSON.parse(data) || {};
+
+        emailEL.value = saved.email ?? "";
+        messageEl.value = saved.message ?? "";
+
+        formData.email = emailEL.value.trim();
+        formData.message = messageEl.value.trim();
+    } catch {
+
+    }
+} 
+
 
